27819655beddd69156757910a3b07437ecc8700b
Nahuel Benitez <nahueljosebenitez7@gmail.com>
2023-12-28 19:31:26 -0300

    doc: CRUD

--- index.js
+++ index.js
@@ -17,14 +17,34 @@ async function main() {
   //   users.map((user) => {
   //     console.log(`${user.id}-${user.name}`);
   //   });
-
-  const userFound = await prisma.user.findFirst({
-    where: {
-      name: "Agustin",
-    },
-  });
-
-  console.log(userFound);
+  // BUSCAR DATO
+  //   const userFound = await prisma.user.findFirst({
+  //     where: {
+  //       name: "Agustin",
+  //     },
+  //   });
+  //   console.log(userFound);
+  // ELIMINAR DATO (si no existe lanza error asi que recordar manejar con trycatch)
+  //   const userDeleted = await prisma.user.delete({
+  //     where: {
+  //       id: 3,
+  //     },
+  //   });
+  //   console.log(userDeleted);
+  //   const users = await prisma.user.findMany();
+  //   console.log(users);
+  //   ACTUALIZAR DATOS (con upsert se actualiza si existe sino se crea)
+  //   const userUpdated = await prisma.user.update({
+  //     where: {
+  //       id: 1,
+  //     },
+  //     data: {
+  //       email: "nahuel@gmail.com",
+  //       name: "Nahuel",
+  //       lastname: "José",
+  //     },
+  //   });
+  //   console.log(userUpdated);
 }
 
 main();
